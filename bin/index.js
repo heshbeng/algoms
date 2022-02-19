ceeb98156f288f8a298dd6bce02c2908b6e9de28
heshbeng <98661332+heshbeng@users.noreply.github.com>
2022-02-18 22:15:02 -0600

    edit help message for clarity

--- bin/index.js
+++ bin/index.js
@@ -17,9 +17,9 @@ From the algoms directory, enter 'npm install -g' to enable the 'algoms' command
 `)
 //.command('msig', 'Sign tx file with multisig wallet')
 //.example('algoms msig -t /path/to/algorand/tx1.tx -t /path/to/algorand/tx2.tx -k "private key 1" -k "private key 2"...', "Signs 'tx1.tx' and 'tx2.tx' with the specified keys")
-.example('algoms -t /path/to/algorand/tx1.tx -t /path/to/algorand/tx2.tx -k "private key 1" -k "private key 2"...', `Signs 'tx1.tx' and 'tx2.tx' with the specified keys
+.example('algoms -t /path/to/algorand/tx1.tx -t /path/to/algorand/tx2.tx -k "private key 1" -k "private key 2"...', `Signs 'tx1.tx' and 'tx2.tx' with the specified keys.
 
-If not specified, the filepath will default to the current working directory.`)
+If the filepath is not specified, it will default to the current working directory.`)
 .option('t', { alias: 'transaction', description: 'Filename of the transaction to be signed (If signing multiple transactions at once, make sure each one has a unique filename)', type: 'string', demandOption: true})
 .option('k', { alias: 'key', description: 'Private key or seed phrase in " "', type: 'string', demandOption: true})
 .argv;
